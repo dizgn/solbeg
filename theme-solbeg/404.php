90e492def8edac394d71b113497daaebef8b9d3f
Dmitry Borovitsky <99206135+dizgn@users.noreply.github.com>
2023-01-24 18:18:52 +0100

    Add files via upload

---
+++ theme-solbeg/404.php
@@ -0,0 +1,8 @@
+﻿<?php require('header.php');?>
+<div align="center" style="position:absolute;left:50%;top:40%;transform: translate(-50%, -50%);">
+	<!-- <a href="<?php echo get_site_url(); ?>"><img width="320" src="<?php echo get_template_directory_uri(); ?>/img/logo_broken.svg"></a> !-->
+	<div class="wrap3"></div>
+	<div class="bigtext">404 Err...</div>
+	<div class="wrap3"></div>
+	<a href="<?php echo get_site_url(); ?>">Back to Main</a>
+</div>
